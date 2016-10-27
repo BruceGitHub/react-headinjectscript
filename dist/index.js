be966671772811e38f6db5705c4a50531a2bd27e
BruceGitHub <yagyog@tiscali.it>
2016-10-27 12:29:50 +0200

    Add files via upload

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,13 @@
+'use strict';
 
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _componentsHeadinjectscript = require('./components/headinjectscript');
+
+var _componentsHeadinjectscript2 = _interopRequireDefault(_componentsHeadinjectscript);
+
+exports.HeadInjectScript = _componentsHeadinjectscript2['default'];
